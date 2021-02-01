0c6fec9c319c1c12651c353df81cc53363bbdef9
Mamta Jha <mamta.jha@techscalable.com>
2021-02-01 06:48:03 +0530

    ingress-fix

--- script.sh
+++ script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 kubectl get nodes -o wide | grep worker | awk '{print $1}'|head -n 1 > node1
-kubectl get nodes -o wide | grep worker | awk '{print $1}'|tail -n 1 > node2
-n1=$(cat node1)
-n2=$(cat node2)
+kubectl get nodes -o wide | grep worker | awk '{print $1}'|head -n 1 > node2
+kubectl get nodes -o wide | grep worker | awk '{print $2}'|tail -n 1 > node3
+n1=$(cat node2)
+n2=$(cat node3)
 ssh ubuntu@$n1 "sudo systemctl stop docker" 
 ssh ubuntu@$n2 "sudo systemctl stop kubelet" 
 
