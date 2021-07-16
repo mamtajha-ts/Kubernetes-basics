0c1eedab170b8c56df40c1928ba1eae58e8f82fc
mamtajha-ts <53425362+mamtajha-ts@users.noreply.github.com>
2021-07-16 08:37:49 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-kubectl get nodes -o wide | grep worker | awk '{print $1}'|head -n 1 > node1
-kubectl get nodes -o wide | grep worker | awk '{print $1}'|head -n 1 > node2
-kubectl get nodes -o wide | grep worker | awk '{print $2}'|tail -n 1 > node3
-n1=$(cat node2)
-n2=$(cat node3)
-ssh ubuntu@$n1 "sudo systemctl stop docker" 
-ssh ubuntu@$n2 "sudo systemctl stop kubelet" 
+ 
+read -p "Enter the worker1 username : " w1
+ 
+read -p "Enter the worker2 username : " w2
+ 
 
+kubectl get nodes | grep -v master | grep -v NAME | head -n 1 | awk '{print $1}' > node1
+ 
+kubectl get nodes | grep -v master | tail -n 1 | awk '{print $1}' > node2
+ 
+
+n1=$(cat node1)
+ 
+n2=$(cat node2)
+ 
+
+ssh $w1@$n1 "sudo systemctl stop docker"
+ 
+ssh $w2@$n2 "sudo systemctl stop kubelet"
